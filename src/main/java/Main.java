096f5e6bd41df1add6b0194d5fd9ca6746545bbf
Jorel Ali <contact@jorel.dev>
2020-07-13 16:47:37 +0100

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import parser.ParseException;
+import parser.Parser;
+
+public class Main {
+	
+	public static void main(String[] args) throws ParseException {
+		Parser.parse("(if hello)");
+		System.out.println("Parsing complete!");
+	}
+	
+}
